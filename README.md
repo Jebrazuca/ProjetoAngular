15c6e172d8f1e1e06961cd123c73376f786281cf
Jebrazuca <je_jss@hotmail.com>
2019-07-03 02:31:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# primeiroangular-ela56y
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/primeiroangular-ela56y)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Mi Max 2',
+    price: 799,
+    description: 'Um grande telefone com uma tela de 6.4 polegadas'
+  },
+  {
+    name: 'Huawei P20',
+    price: 699,
+    description: 'Um excelente telefone com uma das melhores cameras'
+  },
+  {
+    name: 'Moto Z',
+    price: 299,
+    description: 'Celular básico mas com tudo o que precisa'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
